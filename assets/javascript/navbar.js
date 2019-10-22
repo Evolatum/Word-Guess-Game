a9c6687e991bc1fb743e4a5f02c2b051189ed551
Santiago MVO <santiago.mvao@gmail.com>
2019-10-22 17:28:53 -0500

    updated portfolio link

--- assets/javascript/navbar.js
+++ assets/javascript/navbar.js
@@ -1,6 +1,6 @@
 $("body").prepend(`
 <nav class="navbar navbar-dark fixed-top bg-dark">
-    <a class="navbar-brand" href="https://evolatum.github.io/Portfolio/portfolio.html">
+    <a class="navbar-brand" href="https://evolatum.github.io/Portfolio/index.html#linkPortfolio">
         <i class="fad fa-chevron-left d-inline-block align-middle" id="backButton" alt="Go back"></i>
         Back to portfolio
     </a>
